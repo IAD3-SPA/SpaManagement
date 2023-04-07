428f95ca8b7588286415f96dee97bd7f6167c9c9
Jakub Owczarek <jakub.owczarek@codete.com>
2023-04-07 16:19:12 +0200

    Add models to admin.py

--- SpaApp/admin.py
+++ SpaApp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, ProductDelivery, Storage
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductDelivery)
+class ProductDeliveryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Storage)
+class StorageAdmin(admin.ModelAdmin):
+    pass
