a2195694fc996af4948a1d3f4672cff0221f6b64
KarolxSliwa <102217359+KarolxSliwa@users.noreply.github.com>
2023-05-06 00:12:09 +0200

    working databases

--- SpaApp/models.py
+++ SpaApp/models.py
@@ -196,28 +196,6 @@ class Supplier(User):
             self.type = User.Types.SUPPLIER
         return super().save(*args, **kwargs)
 
-'''
-# stary Client nie zmodyfikowany 
-class Client(models.Model): 
-    name = models.CharField(max_length=255) 
-    surname = models.CharField(max_length=255) 
-    phone_number = models.CharField(max_length=15)
-
-    def __str__(self):
-        return self.name + " " + self.surname
-'''
-
-class Appointment(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    date = models.DateField()
-    time = models.TimeField()
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    
-
-    def __str__(self):
-        return self.name
-
 
 class Client(models.Model): 
     name = models.CharField(max_length=255) 
@@ -235,6 +213,18 @@ class Client(models.Model):
     def reduce_benefits_program(self, product_value):
         self.benefits_program -= product_value
         self.save()
+
+
+class Appointment(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
