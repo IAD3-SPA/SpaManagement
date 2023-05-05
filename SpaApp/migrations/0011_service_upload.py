a765d16ed1c4359ee97f5d7196933023c8760225
wojti1728 <wzelasko@student.agh.edu.pl>
2023-05-05 18:38:18 +0200

    some additional changes

--- SpaApp/migrations/0011_service_upload.py
+++ SpaApp/migrations/0011_service_upload.py
@@ -12,10 +12,23 @@ from django.db import migrations, models
 def insert_values(apps, schema_editor):
     service = apps.get_model('SpaApp', 'Service')
     # P7D stands for Period 7 Days
-    service.objects.create(code='SER000', service_name='Oriental Massage', image=None, price=100., description="The treatment begins with a relaxing bath in brightening and improving the condition of the skin Klawska salt containing minerals. It will allow you to relax both body and mind")
-    service.objects.create(code='SER001', service_name='China Traditional Massage', image=None, price=150.5, description="It is based on the concept of Jing Luo /meridians, energy channels/ and the conduction of internal Chi / Qi / energy. It is a pillar of Chinese longevity culture, and its principles of meridians have illuminated centuries of Chinese doctors' paths.")
-    service.objects.create(code='SERT02', service_name='Hot Baths', image=None, price=50., description="It nourishes, smooths and firms the skin of the entire body, giving it a glow and extraordinary softness. A glass of prosecco is served with the bath.")
-    service.objects.create(code='SER003', service_name='Rituals', image=None, price=100.5, description="Classical Thai massage is a combination of acupressure techniques and passive yoga. It uses a mat on which the person being massaged assumes various positions derived from yoga.")
+    service.objects.create(code='SER000', service_name='Oriental Massage', image=None, price=100., description="""The treatment begins with a relaxing bath
+                                                                                                                 in brightening and improving the condition 
+                                                                                                                 of the skin Klawska salt containing minerals. 
+                                                                                                                 It will allow you to relax both body and mind""")
+    service.objects.create(code='SER001', service_name='China Traditional Massage', image=None, price=150.5, description="""It is based on the concept of Jing 
+                                                                                                                            Luo /meridians, energy channels/ and the 
+                                                                                                                            conduction of internal Chi / Qi / energy. 
+                                                                                                                            It is a pillar of Chinese longevity culture, 
+                                                                                                                            and its principles of meridians have illuminated 
+                                                                                                                            centuries of Chinese doctors' paths.""")
+    service.objects.create(code='SERT02', service_name='Hot Baths', image=None, price=50., description="""It nourishes, smooths and firms the skin of the entire body,
+                                                                                                            giving it a glow and extraordinary softness. 
+                                                                                                            A glass of prosecco is served with the bath.""")
+    service.objects.create(code='SER003', service_name='Rituals', image=None, price=100.5, description="""Classical Thai massage is a combination of acupressure 
+                                                                                                            techniques and passive yoga. It uses a mat on which 
+                                                                                                            the person being massaged assumes various 
+                                                                                                            positions derived from yoga.""")
     _add_image(service, 'Oriental Massage', 'media/products/massage_oil.jpg')
     _add_image(service, 'China Traditional Massage', 'media/products/lavender_oil.jpeg')
     _add_image(service, 'Hot Baths', 'media/products/bath_salts.jpg')
@@ -26,14 +39,14 @@ def delete_values(apps, schema_editor):
     service.objects.all().delete()
 
 
-def _add_image(product_model, name, image_path):
+def _add_image(service_model, name, image_path):
     image_real_path = os.path.join('media', 'images', f'{name.replace(" ", "_")}.jpg')
     if os.path.exists(image_real_path):
         return
 
-    product = product_model.objects.get(service_name=name)
+    service = service_model.objects.get(service_name=name)
     with open(image_path, 'rb') as f:
-        product.image.save(f'{name}.jpg', File(f))
+        service.image.save(f'{name}.jpg', File(f))
 
 
 class Migration(migrations.Migration):
